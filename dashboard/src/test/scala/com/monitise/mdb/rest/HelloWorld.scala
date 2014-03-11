62f58ddb61cccbacd360ae2bf3fd228d2c3fa67b
unknown <demirv@LON04-03418.corp.monitise.net>
2014-03-11 10:37:51 +0000

    init

---
+++ dashboard/src/test/scala/com/monitise/mdb/rest/HelloWorld.scala
@@ -0,0 +1,14 @@
+//package com.monitise.mdb.rest
+//
+//import org.scalatest.FlatSpec
+//
+//object HelloWorld extends FlatSpec {
+//
+//  
+//  it should "say Hello Java" in {
+//    assert("Hello Java" === "Hello Java")
+//  
+//  }
+//  
+//  
+//}
